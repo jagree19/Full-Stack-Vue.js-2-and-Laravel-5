6fdfa842ba90c2ca9d9a27ccd6002911cba2b433
Scott Pinkelman <scott@scottpinkelman.com>
2019-07-15 16:46:04 -0600

    chapter7 complete carousel and router functions completed

--- vuebnb/resources/assets/components/HomePage.vue
+++ vuebnb/resources/assets/components/HomePage.vue
@@ -1,26 +1,19 @@
 <template>
     <div class="home-container">
-        <div v-for="(group, country) in listing_groups" class="listing-summary-group">
-            <h1>Places in {{ country }}</h1>
-            <div class="listing-summaries">
-                <listing-summary
-                    v-for="listing in group"
-                    :key="listing.id"
-                    :listing="listing">
-                </listing-summary>
-            </div>
-        </div>
+        <listing-summary-group
+            v-for="(group, country) in listing_groups"
+            :key="country"
+            :listings="group"
+            :country="country"
+            class="listing-summary-group">
+        </listing-summary-group>
     </div>
 </template>
 
 <script>
     import { groupByCountry } from "../js/helpers";
-    import ListingSummary from './ListingSummary.vue';
-    import axios from 'axios';
     import routeMixin from '../js/route-mixin';
-
-    let serverData = JSON.parse(window.vuebnb_server_data);
-    let listing_groups = groupByCountry(serverData.listings);
+    import ListingSummaryGroup from "./ListingSummaryGroup";
 
     export default {
         mixins: [routeMixin],
@@ -35,28 +28,7 @@
             }
         },
         components: {
-            ListingSummary
+            ListingSummaryGroup
         }
     }
 </script>
-
-<style>
-    .listing-summary-group {
-        padding-bottom: 20px;
-    }
-
-    .listing-summaries {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        overflow: hidden;
-    }
-
-    .listing-summaries > .listing-summary {
-        margin-right: 15px;
-    }
-
-    .listing-summaries > .listing-summary:last-child {
-        margin-right: 0;
-    }
-</style>
